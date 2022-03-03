adf50b071baa164252efdc008d8c22dd19de7172
daweiwwwang <daweiwwwang@tencent.com>
2022-03-04 00:06:07 +0800

    增加窗口修改尺寸的支持

--- Vulkan_Study/Vulkan_Study/main.cpp
+++ Vulkan_Study/Vulkan_Study/main.cpp
@@ -1,16 +1,16 @@
 #include "HelloTriangleApplication.h"
+#include "SwapChainRecreation.h"
 
 int main() {
-	HelloTriangleApplication app;
+	//HelloTriangleApplication app;
+	SwapChainRecreation app;
 
 	try {
 		app.run();
-
 	}
 	catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
-
 	}
 
 	return EXIT_SUCCESS;
